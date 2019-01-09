8b8424a669b45756b7118928e53dc56a165bea00
Kyle <kmarquardt@ucvts.org>
2019-01-09 15:48:59 +0000

    Restructured code to be in classes and started process of allowing player to identify what card they have

--- finalproject.py
+++ finalproject.py
@@ -3,8 +3,20 @@ import random
 'player1score' == 0
 'player2score' == 0
 
-def shuffle_cards():
-    deck = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K']
-    random.shuffle_cards(deck)
-    return deck
+class card:
+    def __init__ (self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def cardcolor (self):
+        if self.suit == 'spades' or self.suit == 'clubs':
+            return 'black'
+        else:
+            return 'red'
+
+    def facecard(self):
+
+
+
+
 
