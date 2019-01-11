514958e5cd8666ab58ab3a44923b8309c818bfd8
Kyle <kmarquardt@ucvts.org>
2019-01-11 15:43:57 +0000

    Began to siplify functions to make later coding easier to use and to have less variables and names

--- finalproject.py
+++ finalproject.py
@@ -1,21 +1,35 @@
-import random
+from random import shuffle
 
-'player1score' == 0
-'player2score' == 0
+suite = "H, D, S, C".split()
+ranks = "2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A"
 
-class card:
-    def __init__ (self, suit, value):
-        self.suit = suit
-        self.value = value
+class deck:
+    def __init__ (self,):
+        self.deck
 
-    def cardcolor (self):
-        if self.suit == 'spades' or self.suit == 'clubs':
-            return 'black'
-        else:
-            return 'red'
+
+
 
     def facecard(self):
+        face_cards = [11, 12, 13]
+        for face_card in face_cards:
+            if self.rank == face_card:
+                return true
+            else:
+                return false
 
+    def description(self):
+        rank = self.value
+        if rank == 1:
+            rank = "Ace"
+        elif rank == 11:
+            rank = "Jack"
+        elif rank == 12:
+            rank = "Queen"
+        elif rank == 13:
+            rank = "King"
+
+        return "{rank} of {self.suit}
 
 
 
