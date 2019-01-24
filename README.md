4807a2ae27e07176e4e088dbdc6b584fd98f24b8
Kyle <kmarquardt@ucvts.org>
2019-01-24 13:58:12 +0000

    Finalized comments and finished adding to the readme file

--- README.md
+++ README.md
@@ -4,7 +4,12 @@
 
 *Another small difference between my version of War in python and the version that would typically be played with cards is that when
 you play it in real life with cards you obviously would have a second player to play with. In my version of the game, I programmed
-an AI player that you play against and try to beat in order to win the game.
+an AI player that you play against and try to beat in order to win the game. It is also important to note that my game does not have
+much actual gameplay due to the fact that war is more of a game of chance rather than a game of skill so there are no actual decisions
+that you can make in the game because you just draw random cards each round. Due to this fact, when you run my game it runs through the
+entire game instantaneously and gives you a very detailed description of each round, who won the game, how many times war was played, etc.
+The reason that I did not incorporate a function to allow the player to draw their own card is because War can technically go on forever
+so if I did that the game may never end and the player would get bored or their computer would crash before they finish the game.
 
 *The basic rules of War are that a standard 52 card deck is split in half between two players and each player has a hand of 26 cards that
 are randomly ordered and face down so that the player cannot see their own cards. At the start of each round, each player flips over the
@@ -13,5 +18,24 @@ cards and adds them to the bottom of their hand. When two players draw the same 
 three more cards face down, and then flip over a fourth card and these are the cards that they compare values of. If each player's fourth
 card have the same value as each other, they enter double war and repeat the same war process until a winner is determined. The game is
 over once one player gains all 52 cards in the deck, meaning that technically this game can last for an infinite number of rounds.
+
+*While programming my game, I used a few sources for reference in how to set up my code and used some other resources including my friend
+to learn a few new python commands that helped me simplify my code and make it work more smoothly
+
+*Some of the new python commands that I learned to use were "from random import shuffle", ".split()", "len()", "index()", "try:" and "except:"
+
+*When using these new commands that I had to learn, the most difficult to incorporate was the try and except command because it was somewhat
+confusing to understand where exactly to use it. Originally in my code I was using if, elif, and else exclusively but in some places that didn't
+work very well (the double war section), so I consulted a friend who knows a decent amount about coding and he told me to try using try and except
+to replicate a similar effect so that if the code in the try block did not work then it would advance down to the except block and run that instead.
+
+Resources:
+https://www.pythonforbeginners.com/error-handling/python-try-and-except
+https://docs.python.org/3/genindex-all.html
+https://stackoverflow.com/questions/19804154/war-card-game-help-python
+
+Flowchart: https://drive.google.com/file/d/11IWyBOLD9niKOVH93YpSarIgoDQZc3fH/view?usp=sharing
+
+
 
 
--- finalproject.py
+++ finalproject.py
@@ -4,42 +4,55 @@ suite = "H, D, S, C".split()
 ranks = "2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A".split()
 
 class deck:
+    """Class describing the entire deck of 52 cards"""
     def __init__ (self,):
+        """Creates the actual deck of cards with the correct values and suites"""
         self.deck = [(s,r) for s in suite for r in ranks ]
         print ("New deck created...")
 
     def split(self):
+        """Splits the deck into 2 halves"""
         return (self.deck[:26],self.deck[26:])
 
     def shuffle(self):
+        """Shuffles the deck into a random order"""
         shuffle(self.deck)
         print("Deck shuffled...")
 
 class hand:
+    """Class describing each players hand"""
     def __init__(self,cards):
+        """Defines each players hand of cards"""
         self.cards = cards
 
     def add_card(self, added_cards):
+        """Function for adding cards to each hand"""
         self.cards.extend(added_cards)
 
     def remove_card(self):
+        """Function for removing cards from each hand"""
         return self.cards.pop()
 
     def __str__(self):
+        """Function to tell each player the score/ how many cards they have left"""
         return "Has {} cards left". format(len(self.name, drawn_card))
 
 class Player:
+    """Class describing the player of the game I.e you!"""
     def __init__(self, hand, name):
+        """Creates variables for the player"""
         self.hand = hand
         self.name = name
 
     def play_card(self):
+        """Function that removes a card from each hand and tells what cards were played by each party"""
         drawn_card = self.hand.remove_card()
         print("{} has played: {}".format(self.name,drawn_card))
         print("\n")
         return drawn_card
 
     def remove_war_cards (self):
+        """Function comparing and then removing the cards from each respective party"""
         war_cards = []
         if len(self.hand.cards) < (3):
             return war_cards
@@ -49,28 +62,31 @@ class Player:
                 return war_cards
 
     def cards_remaining(self):
+        """Function that keeps track of how many cards are remaining"""
         return len(self.hand.cards) != 0
 
 print("Welcome to War!")
 print("In this game you will randomly draw cards from a shuffled deck to 'wage war'")
 print("against a UNSC Strategic Artifical Intelligence Construct named Cortana.")
 print("Are you ready to wage war?")
+#Introduces the player to the game and gives them a brief set of rules
 
 d = deck()
 d.shuffle()
 split1, split2 = d.split()
-
+#Runs the functions to create the decks and then split and shuffle them
 
 
 ai = Player(hand(split1),"Cortana")
 name = input("Enter Your Name: ")
 user = Player(hand(split2),name)
-
+#Defines the names of the players and their corresponding variables
 
 total_rounds = 0
 number_of_war= 0
 double_war =0
 except_count =0
+#Sets all of the counters to 0 to start the game
 
 while user.cards_remaining() and ai.cards_remaining():
     total_rounds += 1
@@ -78,14 +94,16 @@ while user.cards_remaining() and ai.cards_remaining():
     print("Current score:")
     print(user.name+" has "+str(len(user.hand.cards))+" cards left")
     print(ai.name+" has "+str(len(ai.hand.cards))+" cards left")
-    print("You card will now be played for you")
+    print("Your card will now be played for you")
     print("")
+#Introduces the player to each new round
 
     cards_in_play = []
     ai_card = ai.play_card()
     player_card = user.play_card()
     cards_in_play.append(ai_card)
     cards_in_play.append(player_card)
+#Runs the functions that chooses cards from each party and compares them
 
     if ai_card[1] == player_card[1]:
         number_of_war +=1
@@ -93,7 +111,7 @@ while user.cards_remaining() and ai.cards_remaining():
         print("Commencing war!")
         cards_in_play.extend(user.remove_war_cards())
         cards_in_play.extend(ai.remove_war_cards())
-
+#Defines when to have a war to break a tie
         try:
             ai_card = ai.play_card()
             player_card = user.play_card()
@@ -103,11 +121,13 @@ while user.cards_remaining() and ai.cards_remaining():
 
             if ranks.index(ai_card[1]) < ranks.index(player_card[1]):
                 user.hand.add_card(cards_in_play)
+#Defines when a normal war is won
             elif ranks.index(ai_card[1]) == ranks.index(player_card[1]):
                 double_war +=1
                 print("Wow you have engaged in a double war!")
                 cards_in_play.extend(user.remove_war_cards())
                 cards_in_play.extend(ai.remove_war_cards())
+#Defines when there is a double war to break a tie in a normal war
                 try:
                     ai_card = ai.play_card()
                     player_card = user.play_card()
@@ -119,18 +139,18 @@ while user.cards_remaining() and ai.cards_remaining():
 
                     else:
                         ai.hand.add_card(cards_in_play)
-
+#Defines how a double war is won
                 except:
                     except_count =+1
                     print("There are not enough cards for double war!")
                     break
             else:
                 ai.hand.add_card(cards_in_play)
-
         except:
             except_count +=1
             print("There are not enough cards in the deck for a war! Game over!")
             break;
+#Decides whether or not a war or double war is possible with the amount of cards remaining
     else:
         if ranks.index(ai_card[1]) < ranks.index(player_card[1]):
             user.hand.add_card(cards_in_play)
@@ -150,6 +170,7 @@ print("In this game, there were "+str(total_rounds)+" rounds!")
 print("There were "+str(number_of_war)+" rounds of war in this game.")
 print("There were "+str(double_war)+" rounds of double war in this game.")
 print("Amount of war games that exceeded double war: "+str(except_count))
+#List of if and else statements that determine who wins each round and who wins each game and provides a message for each respective condition
 
 
 
